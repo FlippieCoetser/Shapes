9b73577c8f3e57488ea23f88f8b4db187c2461d7
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 05:18:13 -0700

    GENERATOR: specifications |> Rectangle.Coordinates.Generator() ReturnCoordinatesWithPair3XEqualWidthAndYEqualHeight -> PASS

--- R/Rectangle.Coordinates.Generator.R
+++ R/Rectangle.Coordinates.Generator.R
@@ -1,4 +1,4 @@
 Rectangle.Coordinates.Generator <- \(specifications) data.frame(
-  x = c(0,specifications[['width']],0,0,0),
-  y = c(0,0,0,0,0)
+  x = c(0,specifications[['width']],specifications[['width']],0,0),
+  y = c(0,0,specifications[['height']],0,0)
 )
