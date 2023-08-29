448dc93e3957f4f1c5c49289f4437b49434f7865
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 04:57:02 -0700

    GENERATOR: specifications |> Rectangle.Coordinates.Generator() ReturnCoordinatesWith5XAndYValues -> FAIL

--- tests/testthat/test-Rectangle.Coordinates.Generator.R
+++ tests/testthat/test-Rectangle.Coordinates.Generator.R
@@ -5,7 +5,7 @@ describe('Rectangle.Coordinates.Generator',{
 })
 
 describe('When coordinates <- specifications |> Rectangle.Coordinates.Generator()',{
-  it("Then Coordinates has x and y values",{
+  it("Then coordinates has x and y values",{
     # Given
     specifications <- list()
     specifications[['width']] <- 20
@@ -17,5 +17,19 @@ describe('When coordinates <- specifications |> Rectangle.Coordinates.Generator(
     # Then
     coordinates[['x']] |> expect.exist()
     coordinates[['y']] |> expect.exist()
+  })
+  it("Then coordinates has 5 pairs of x and y values",{
+    # Given
+    specifications <- list()
+    specifications[['width']] <- 20
+    specifications[['height']] <- 10
+
+    expected.pairs <- 5
+
+    # When
+    coordinates <- specifications |> Rectangle.Coordinates.Generator()
+
+    # Then
+    coordinates |> expect.rows(expected.pairs)
   })
 })
