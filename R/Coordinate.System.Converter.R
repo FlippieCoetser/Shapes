2d63a9b1b0f99dfcc80e8a29b476cbfa5727ffba
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 08:51:07 -0700

    CONVERTER: coordinates |> convert[['PolarToCartesian']]() ReturnsCartesianCoordinates -> PASS

--- R/Coordinate.System.Converter.R
+++ R/Coordinate.System.Converter.R
@@ -1,6 +1,11 @@
 Coordinate.System.Converter <- \() {
   converters <- list()
-  converters[['PolarToCartesian']] <- \() {}
+  converters[['PolarToCartesian']] <- \(coordinates) {
+    data.frame(
+      x = (coordinates[['angle']] |> cos()) * coordinates[['radius']],
+      y = (coordinates[['angle']] |> sin()) * coordinates[['radius']]
+    )
+  }
   converters[['CartesianToPolar']] <- \() {}
   return(converters)
 }
