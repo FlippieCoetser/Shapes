82ea2bc4f95d83027a648b8d48b478d8eb6cfa4c
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 09:39:20 -0700

    GENERATOR: specifications |> Circle.Coordinates.Generator() ReturnCoordinatesWith37XandYValues -> PASS

--- R/Circle.Coordinates.Generator.R
+++ R/Circle.Coordinates.Generator.R
@@ -1,4 +1,9 @@
-Circle.Coordinates.Generator <- \(specifications) data.frame(
-  x = '',
-  y = ''
-)
+Circle.Coordinates.Generator <- \(specifications) {
+  angle <- seq(0,360, 10)
+  radius <- 1 |> rep(length(angle))
+
+  data.frame(
+    x = 1:length(angle),
+    y = 1:length(radius)
+  )
+}
