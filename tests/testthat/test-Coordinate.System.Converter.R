61e9c3876a7b76cfe9f0636745c92422f49c9f22
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 08:50:43 -0700

    CONVERTER: coordinates |> convert[['PolarToCartesian']]() ReturnsCartesianCoordinates -> FAIL

--- tests/testthat/test-Coordinate.System.Converter.R
+++ tests/testthat/test-Coordinate.System.Converter.R
@@ -27,3 +27,55 @@ describe('When converters <- Coordinate.System.Converter()',{
     converters[['CartesianToPolar']] |> expect.function()
   })
 })
+
+describe('When coordinates |> convert[["PolarToCartesian"]]()',{
+  it('then a data.frame with x and y is returned',{
+    # Given
+    convert <- Coordinate.System.Converter()  
+
+    coordinates <- list()
+    coordinates[['radius']] <- 1
+    coordinates[['angle']]  <- 0   
+   
+    # When
+    actual.coordinates <- coordinates |> convert[['PolarToCartesian']]() 
+     
+    # Then
+    actual.coordinates |> expect.data.frame() 
+
+    actual.coordinates[['x']] |> expect.exist() 
+    actual.coordinates[['y']] |> expect.exist() 
+  })
+  it('then x is equal to radius * cos(angle)',{
+    # Given
+    convert <- Coordinate.System.Converter()     
+
+    coordinates <- list()
+    coordinates[['radius']] <- 1
+    coordinates[['angle']]  <- 0
+
+    expected.x <- (coordinates[['angle']] |> cos()) * coordinates[['radius']] 
+          
+    # When
+    actual.coordinates <- coordinates |> convert[['PolarToCartesian']]() 
+     
+    # Then
+    actual.coordinates[['x']] |> expect_equal(expected.x)
+  })
+  it('then y is equal to radius * sin(angle)',{
+    # Given
+    convert <- Coordinate.System.Converter()     
+
+    coordinates <- list()
+    coordinates[['radius']] <- 1
+    coordinates[['angle']]  <- 0
+
+    expected.y <- (coordinates[['angle']] |> sin()) * coordinates[['radius']] 
+          
+    # When
+    actual.coordinates <- coordinates |> convert[['PolarToCartesian']]() 
+     
+    # Then
+    actual.coordinates[['y']] |> expect_equal(expected.y)
+  })
+})
