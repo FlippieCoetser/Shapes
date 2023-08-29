7b18fe41c1578242ee1b03efcb9b6a7e857ac1fe
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 04:54:14 -0700

    GENERATOR: specifications |> Rectangle.Coordinates.Generator() ReturnCoordinatesWithXAndYValue -> FAIL

--- tests/testthat/test-Rectangle.Coordinates.Generator.R
+++ tests/testthat/test-Rectangle.Coordinates.Generator.R
@@ -3,3 +3,19 @@ describe('Rectangle.Coordinates.Generator',{
     Rectangle.Coordinates.Generator |> expect.exist()
   })
 })
+
+describe('When coordinates <- specifications |> Rectangle.Coordinates.Generator()',{
+  it("Then Coordinates has x and y values",{
+    # Given
+    specifications <- list()
+    specifications[['width']] <- 20
+    specifications[['height']] <- 10
+
+    # When
+    coordinates <- specifications |> Rectangle.Coordinates.Generator()
+
+    # Then
+    coordinates[['x']] |> expect.exist()
+    coordinates[['y']] |> expect.exist()
+  })
+})
