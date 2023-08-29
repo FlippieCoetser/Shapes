128451650fa6c5b984bb3b6c66a358b25210ea4d
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 09:28:20 -0700

    GENERATOR: specifications |> Circle.Coordinates.Generator() ReturnCoordinatesWithXAndYValues -> FAIL

--- tests/testthat/test-Circle.Coordinates.Generator.R
+++ tests/testthat/test-Circle.Coordinates.Generator.R
@@ -3,3 +3,18 @@ describe('Circle.Coordinates.Generator',{
     Circle.Coordinates.Generator |> expect.exist()
   })
 })
+
+describe('When coordinates <- specifications |> Circle.Coordinates.Generator()',{
+  it("Then coordinates has x and y values",{
+    # Given
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    # When
+    coordinates <- specifications |> Circle.Coordinates.Generator()
+
+    # When
+    coordinates[['x']] |> expect.exist()
+    coordinates[['y']] |> expect.exist()
+  })
+})
