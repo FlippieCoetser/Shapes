541ebf663d00cf1a83dfeed8aff0466a7d1b17fe
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 11:40:00 -0700

    GENERATOR: specifications |> generate[['Rectangle']]() ReturnsCoordinatesOfRectangleWithSpecifiedWidthAndHeight -> PASS

--- R/Shape.Coordinates.Generator.R
+++ R/Shape.Coordinates.Generator.R
@@ -1,6 +1,11 @@
 Shape.Coordinates.Generator <- \() {
   generators <- list()
-  generators[['Rectangle']] <- \() {}
+  generators[['Rectangle']] <- \(specifications) {
+    data.frame(
+      x = c(0,specifications[['width']],specifications[['width']],0,0),
+      y = c(0,0,specifications[['height']],specifications[['height']],0)
+    )
+  }
   generators[['Circle']] <- \() {}
   return(generators)
 }
