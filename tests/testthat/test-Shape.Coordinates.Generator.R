a4a92b666e596dba0210c83e7e2ce96f5eabc4c5
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 11:43:54 -0700

    GENERATOR: specifications |> generate[['Circle']]() ReturnsCoordinatesOfCircleWithSpecifiedRadius -> FAIL

--- tests/testthat/test-Shape.Coordinates.Generator.R
+++ tests/testthat/test-Shape.Coordinates.Generator.R
@@ -161,3 +161,70 @@ describe('When coordinates <- specifications |> generate[["Rectangle"]]()',{
     coordinates[['y']][pair.number] |> expect.equal(expected.y)
   })
 })
+
+describe('When coordinates <- specifications |> Circle.Coordinates.Generator()',{
+  it("Then coordinates has x and y values",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    # When
+    coordinates <- specifications |> generate[['Circle']]()
+
+    # When
+    coordinates[['x']] |> expect.exist()
+    coordinates[['y']] |> expect.exist()
+  })
+  it("Then coordinates has 37 pairs of x and y values",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    expected.pairs <- 37
+
+    # When
+    coordinates <- specifications |> generate[['Circle']]()
+
+
+    # When
+    coordinates |> expect.rows(expected.pairs)
+  })
+  it("Then the x values is equal to the cosine of the angle in radians times the radius",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+    angle.converter <- Angle.Converter()
+
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    expected.x <- (seq(0,360, 10) |> angle.converter[['DegreesToRadians']]() |> cos()) * specifications[['radius']]
+
+    # When
+    coordinates <- specifications |> generate[['Circle']]()
+
+
+    # When
+    coordinates[['x']] |> expect.equal(expected.x)
+  })
+  it("Then the y values is equal to the sine of the angle in radians times the radius",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+    angle.converter <- Angle.Converter()
+
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    expected.y <- (seq(0,360, 10) |> angle.converter[['DegreesToRadians']]() |> sin()) * specifications[['radius']]
+
+    # When
+    coordinates <- specifications |> generate[['Circle']]()
+
+
+    # When
+    coordinates[['y']] |> expect.equal(expected.y)
+  })
+})
