574fe052fb5fb1e82dc25ec57f66827ed3431715
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-29 09:50:49 -0700

    GENERATOR: specifications |> Circle.Coordinates.Generator() ReturnYCoordinatesEqualSineAngleInRadiansTimesRadius -> FAIL

--- tests/testthat/test-Circle.Coordinates.Generator.R
+++ tests/testthat/test-Circle.Coordinates.Generator.R
@@ -45,4 +45,19 @@ describe('When coordinates <- specifications |> Circle.Coordinates.Generator()',
     # When
     coordinates[['x']] |> expect.equal(expected.x)
   })
+  it("Then the y values is equal to the sine of the angle in radians times the radius",{
+    # Given
+    angle.converter <- Angle.Converter()
+
+    specifications <- list()
+    specifications[['radius']] <- 1
+
+    expected.y <- (seq(0,360, 10) |> angle.converter[['DegreesToRadians']]() |> sin()) * specifications[['radius']]
+
+    # When
+    coordinates <- specifications |> Circle.Coordinates.Generator()
+
+    # When
+    coordinates[['y']] |> expect.equal(expected.y)
+  })
 })
