dc9bd5e3b6d71e7e4f89be6cae155ac859586969
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-22 02:46:19 -0700

    CONFIGURATOR: coordinates |> align[['center']]() ReturnsCenteredCoordinates -> PASS

--- R/Alignment.Configurator.R
+++ R/Alignment.Configurator.R
@@ -1,9 +1,18 @@
 Alignment.Configurator <- \() {
+  shape <- Shape.Utility()
+
   alignment <- list()
   alignment[['corner']]     <- \(coordinates) {
     coordinates
   }
-  alignment[['origin']]     <- \() {}
+  alignment[['center']]     <- \(coordinates) {
+    width  <- coordinates |> shape[['get.width']]()
+    height <- coordinates |> shape[['get.height']]()
+
+    offset <- data.frame(x = -width / 2, y = -height / 2)
+
+    coordinates |> shape[['translate']](offset)
+  }
   alignment[['horizontal']] <- \() {}
   alignment[['vertical']]   <- \() {}
   return(alignment)
