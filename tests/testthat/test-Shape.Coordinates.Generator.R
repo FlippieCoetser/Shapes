e1894c2ea2c8079120df1cd9365898ca590e39ae
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-22 09:16:13 -0700

    GENERATOR: specifications |> generator[['Trapezoid']]() ReturnsSecondPairWithCorrectXAndYEqual0 -> FAIL

--- tests/testthat/test-Shape.Coordinates.Generator.R
+++ tests/testthat/test-Shape.Coordinates.Generator.R
@@ -643,4 +643,48 @@ describe("When coordinates <- specifications |> generate[['Trapezoid']]()",{
     coordinates[['x']][pair.number] |> expect.equal(expected.x)
     coordinates[['y']][pair.number] |> expect.equal(expected.y)
   })
+  it("then pair 2 has x = bottom and y = 0 if trapezoid is descending",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+
+    specifications <- list()
+    specifications[['bottom']] <- 1
+    specifications[['top']]    <- 0.5
+    specifications[['height']] <- 1
+
+    pair.number <- 2
+
+    expected.x <- specifications[['bottom']]
+    expected.y <- 0
+
+    # When
+    coordinates <- specifications |> generate[['Trapezoid']]()
+
+    # Then
+    coordinates[['x']][pair.number] |> expect.equal(expected.x)
+    coordinates[['y']][pair.number] |> expect.equal(expected.y)
+  })
+  it("then pair 2 has x = bottom + difference / 2 and y = 0 if trapezoid is ascending",{
+    # Given
+    generate <- Shape.Coordinates.Generator()
+
+    specifications <- list()
+    specifications[['bottom']] <- 0.5
+    specifications[['top']]    <- 1
+    specifications[['height']] <- 1
+
+    pair.number <- 2
+
+    difference <- (specifications[['bottom']] - specifications[['top']]) |> abs()
+
+    expected.x <- specifications[['bottom']] + difference / 2
+    expected.y <- 0
+
+    # When
+    coordinates <- specifications |> generate[['Trapezoid']]()
+
+    # Then
+    coordinates[['x']][pair.number] |> expect.equal(expected.x)
+    coordinates[['y']][pair.number] |> expect.equal(expected.y)
+  })
 })
