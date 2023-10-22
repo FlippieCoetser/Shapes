1acd2f331a76136cccf62ee7ac64c4ebce9bd866
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-22 02:29:21 -0700

    UTILITY: coordinates |> shape[['get.width']]() ReturnsDifferenceBetweenMaxAndMinXCoordinates -> PASS

--- R/Shape.Utility.R
+++ R/Shape.Utility.R
@@ -1,6 +1,10 @@
 Shape.Utility <- \() {
   utilities <- list()
-  utilities[['get.with']]   <- \() {}
+  utilities[['get.width']]  <- \(coordinates) {
+    max.x <- coordinates[['x']] |> max()
+    min.x <- coordinates[['x']] |> min()
+    (max.x - min.x)
+  }
   utilities[['get.height']] <- \() {}
   return(utilities)
 }
