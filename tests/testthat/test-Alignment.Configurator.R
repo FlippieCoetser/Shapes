5d88a3e5643450f4ba15f153845101f7cbbb7e13
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-22 02:04:25 -0700

    CONFIGURATOR: Alignment.Configurator() ReturnsList -> FAIL

--- tests/testthat/test-Alignment.Configurator.R
+++ tests/testthat/test-Alignment.Configurator.R
@@ -3,3 +3,13 @@ describe('Alignment.Configurator',{
     Alignment.Configurator |> expect.exist()
   })
 })
+
+describe("When configurator <- Alignment.Configurator()",{
+  it("then configurator is a list",{
+    # When
+    configurator <- Alignment.Configurator()
+
+    # Then
+    configurator |> expect.list()
+  })
+})
