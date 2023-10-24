0a06a523286b9c892a772706a7d9c817fb5a43c2
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-24 05:41:02 -0700

    UTILITY: Shape.Utility() ReturnsListContainingShrink.HeightUtility -> PASS

--- R/Shape.Utility.R
+++ R/Shape.Utility.R
@@ -10,21 +10,22 @@
 #' @export
 Shape.Utility <- \() {
   utilities <- list()
-  utilities[['Get.Width']]  <- \(coordinates) {
+  utilities[['Get.Width']]     <- \(coordinates) {
     max.x <- coordinates[['x']] |> max()
     min.x <- coordinates[['x']] |> min()
     (max.x - min.x)
   }
-  utilities[['Get.Height']] <- \(coordinates) {
+  utilities[['Get.Height']]    <- \(coordinates) {
     max.y <- coordinates[['y']] |> max()
     min.y <- coordinates[['y']] |> min()
     (max.y - min.y)
   }
-  utilities[['Translate']]  <- \(coordinates, offset) {
+  utilities[['Translate']]     <- \(coordinates, offset) {
     data.frame(
       x = coordinates[['x']] + offset[['x']],
       y = coordinates[['y']] + offset[['y']] 
     )
   }
+  utilities[['Shrink.Height']] <- \() {}
   return(utilities)
 }
