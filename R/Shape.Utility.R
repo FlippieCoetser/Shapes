d3e0ed465e1ae0ef7fa6b0127244b6a8f690b8f9
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-25 05:41:51 -0700

    UTILITY: coordinates.one |> shape[['Join']](coordinates.two) JoinCoordinates.oneAndCoordinates.two -> PASS

--- R/Shape.Utility.R
+++ R/Shape.Utility.R
@@ -39,6 +39,22 @@ Shape.Utility <- \() {
 
     coordinates
   }
-  utilities[['Join']]          <- \() {}
+  utilities[['Join']]          <- \(coordinates.one, coordinates.two) {
+    intersection <-
+      coordinates.one |>
+        dplyr::mutate(id = 1:dplyr::n()) |>
+        dplyr::inner_join(coordinates.two, by = c('x','y')) |>
+        unique()
+
+    intersection.start <- intersection[which.min(intersection[['id']]),3]
+    intersection.end   <- intersection[which.max(intersection[['id']]),3]
+
+    start <- coordinates.one[0:intersection.start,]
+    end   <- coordinates.one[-(0:(intersection.end-1)),]
+
+    addon <- coordinates.two |> dplyr::anti_join(intersection, by = c('x','y'))    
+
+    start |> rbind(addon) |> rbind(end)
+  }
   return(utilities)
 }
