ee569841e91b3976e14a30ab38ba3b26045d3404
Flippie Coetser <flippie.coetser@gmail.com>
2024-05-06 06:17:01 -0700

    VALIDATOR: validators <- Shape.Validator() ReturnsValidatorsContainingHasHeightValidator -> PASS

--- R/Shape.Validator.R
+++ R/Shape.Validator.R
@@ -1,12 +1,13 @@
 Shape.Validator <- \() {
   validators <- Validate::Validator()
-  validators[['Rectangle']] <- \(specifications) {
+  validators[['Rectangle']]  <- \(specifications) {
     specifications |> validators[['is.not.NULL']]('Rectangle Specifications')
   }
-  validators[['Triangle']]  <- \() {}
-  validators[['Circle']]    <- \() {}
-  validators[['Trapezoid']] <- \() {}
-  validators[['Segment']]   <- \() {}
-  validators[['has.width']] <- \() {}
+  validators[['Triangle']]   <- \() {}
+  validators[['Circle']]     <- \() {}
+  validators[['Trapezoid']]  <- \() {}
+  validators[['Segment']]    <- \() {}
+  validators[['has.width']]  <- \() {}
+  validators[['has.height']] <- \() {}
   return(validators)
 }
