e9467d901cd744c62cbce8f8d6f96a41928f7b38
Flippie Coetser <flippie.coetser@gmail.com>
2024-05-06 06:32:13 -0700

    EXCEPTIONS: exceptions <- Shape.Validation.Exceptions() ThenExceptionsContainsCircle.NULL Exception -> PASS

--- R/Shape.Validation.Exceptions.R
+++ R/Shape.Validation.Exceptions.R
@@ -2,5 +2,6 @@ Shape.Validation.Exceptions <- \() {
   exceptions <- list()
   exceptions[['Rectangle.NULL']] <- \() {}
   exceptions[['Triangle.NULL']]  <- \() {}
+  exceptions[['Circle.NULL']]    <- \() {}
   return(exceptions)
 }
