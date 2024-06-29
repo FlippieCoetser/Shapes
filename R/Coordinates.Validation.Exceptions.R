e4bebec1aa7def0fe7bd589330e6b8c7d83ac0e9
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 04:58:10 -0700

    EXCEPTIONS: input |> exception[['Coordinates.NULL']]() ThrowsExceptionIfInputTRUE -> PASS

--- R/Coordinates.Validation.Exceptions.R
+++ R/Coordinates.Validation.Exceptions.R
@@ -1,5 +1,7 @@
 Coordinates.Validation.Exceptions <- \() {
   exceptions <- list() 
-  exceptions[['Coordinates.NULL']] <- \() { } 
+  exceptions[['Coordinates.NULL']] <- \(invoke) {
+    if(invoke) stop('Coordinates.NULL: Coordinates does not exist.', call. = FALSE)
+   } 
   return(exceptions)
 }
