7ab895d0c95eec07f70c66fc3ef7acada52a1ce4
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 06:18:45 -0700

    VALIDATOR: offset |> validate[['Offset']]() ThrowsExceptionIfOffsetIsInvalid -> PASS

--- R/Offset.Validator.R
+++ R/Offset.Validator.R
@@ -2,17 +2,23 @@ Offset.Validator <- \() {
   exceptions <- Offset.Validation.Exceptions()
 
   validators <- Validate::Validator()
-  validators[['exists']]      <- \(offset, exception) { 
+  validators[['Offset']] <- \(offset) {
+    offset |> validators[['exists']]('Offset.NULL') 
+    offset |> validators[['has.x']]()
+    offset |> validators[['has.y']]()
+    return(offset)
+  }
+  validators[['exists']] <- \(offset, exception) { 
     offset |> validators[['is.not.NULL']]('') |> 
       tryCatch(error = \(...){ TRUE |> exceptions[[exception]]()})
     return(offset)
   }
-  validators[['has.x']]       <- \(offset) {
+  validators[['has.x']]  <- \(offset) {
     offset[['x']] |> validators[['is.not.NULL']]('') |>
       tryCatch(error = \(...){ TRUE |> exceptions[['Attribute.NULL']]('x')})
     return(offset)
   }
-  validators[['has.y']]       <- \(offset) {
+  validators[['has.y']]  <- \(offset) {
     offset[['y']] |> validators[['is.not.NULL']]('') |>
       tryCatch(error = \(...){ TRUE |> exceptions[['Attribute.NULL']]('y')})
     return(offset)
