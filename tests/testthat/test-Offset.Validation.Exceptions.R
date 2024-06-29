96ed53b72c6731567d8ec2c673f95f1378d742d1
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:59:55 -0700

    EXCEPTIONS: input |> exception[['Offset.NULL']]() ThrowsExceptionIfINputIsTRUE -> FAIL

--- tests/testthat/test-Offset.Validation.Exceptions.R
+++ tests/testthat/test-Offset.Validation.Exceptions.R
@@ -27,3 +27,30 @@ describe("When exceptions <- Offset.Validation.Exceptions()",{
     exceptions[['Attribute.NULL']] |> expect.function()
   })
 })
+
+describe("When input |> exceptions[['Offset.NULL']]()",{
+  it("then an exceptions is thrown if input is FALSE",{
+    # GIVEN
+    exceptions <- Offset.Validation.Exceptions()
+
+
+    # WHEN
+    input <- FALSE
+
+    # THEN
+    input |> exceptions[['Offset.NULL']]() |> expect.no.error()
+  })
+  it("then an exceptions is thrown if input is TRUE",{
+    # GIVEN
+    exceptions <- Offset.Validation.Exceptions()
+
+
+    expected.exception <- 'Offset.NULL: Offset does not exist.'
+
+    # WHEN
+    input <- TRUE
+
+    # THEN
+    input |> exceptions[['Offset.NULL']]() |> expect.error(expected.exception)
+  })
+})
