ad54bd2deb44d842e302dd02229122ef1ef173a9
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:06:02 -0700

    VALIDATOR: coordinates |> validate[['exists']]('Coordinates.NULL') ThrowsCoordinatesNULLExceptionIfCoordinatesIsNULL -> PASS

--- R/Coordinates.Validator.R
+++ R/Coordinates.Validator.R
@@ -1,5 +1,11 @@
 Coordinates.Validator <- \() {
-  validators <- list()
-  validators[['exists']]     <- \() { }
+  exceptions <- Coordinates.Validation.Exceptions()
+
+  validators <- Validate::Validator()
+  validators[['exists']]     <- \(coordinates, exception) { 
+    coordinates |> validators[['is.not.NULL']]('') |> 
+      tryCatch(error = \(...){ TRUE |> exceptions[[exception]]()})
+    return(coordinates)
+  }
   return(validators)
 }
