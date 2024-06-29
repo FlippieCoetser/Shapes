76231fd3af3bfd1ea4d6f9a12f4b50f08ad77fdc
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:22:05 -0700

    VALIDATOR: Coordinates.Validator() ReturnsListContainingCoordinatesValidator -> PASS

--- R/Coordinates.Validator.R
+++ R/Coordinates.Validator.R
@@ -2,17 +2,18 @@ Coordinates.Validator <- \() {
   exceptions <- Coordinates.Validation.Exceptions()
 
   validators <- Validate::Validator()
-  validators[['exists']]     <- \(coordinates, exception) { 
+  validators[['Coordinates']] <- \() {}
+  validators[['exists']]      <- \(coordinates, exception) { 
     coordinates |> validators[['is.not.NULL']]('') |> 
       tryCatch(error = \(...){ TRUE |> exceptions[[exception]]()})
     return(coordinates)
   }
-  validators[['has.x']]      <- \(coordinates) {
+  validators[['has.x']]       <- \(coordinates) {
     coordinates[['x']] |> validators[['is.not.NULL']]('') |>
       tryCatch(error = \(...){ TRUE |> exceptions[['Attribute.NULL']]('x')})
     return(coordinates)
   }
-  validators[['has.y']]      <- \(coordinates) {
+  validators[['has.y']]       <- \(coordinates) {
     coordinates[['y']] |> validators[['is.not.NULL']]('') |>
       tryCatch(error = \(...){ TRUE |> exceptions[['Attribute.NULL']]('y')})
     return(coordinates)
