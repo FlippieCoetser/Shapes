7786bf7f4bff525c641b5fd5d5a73b361282a310
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:42:29 -0700

    MODEL: data |> Offset.Model() ReturnsValidOffset -> PASS

--- R/Offset.Mode.R
+++ R/Offset.Mode.R
@@ -1,1 +1,9 @@
-Offset.Model <- \() {}
+Offset.Model <- \(data) {
+  if(data[['x']] |> is.null()) data[['x']] <- 0
+  if(data[['y']] |> is.null()) data[['y']] <- 0
+  
+  data.frame(
+    x = data[['x']],
+    y = data[['y']] 
+  )
+}
