9421e523cea7b49dcf66fb37a42c58f86374bf4f
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:14:16 -0700

    EXCEPTIONS: input |> validate[['Attribute.NULL']]('attribute') ThrowsExceptionIfInputIsTRUE -> FAIL

--- tests/testthat/test-Coordinates.Validation.Exceptions.R
+++ tests/testthat/test-Coordinates.Validation.Exceptions.R
@@ -54,3 +54,30 @@ describe("When input |> exceptions[['Coordinates.NULL']]()",{
     input |> exceptions[['Coordinates.NULL']]() |> expect.error(expected.exception)
   })
 })
+
+describe("When input |> exceptions[['Attribute.NULL']]('attribute')",{
+  it("then an exceptions is thrown if input is FALSE",{
+    # GIVEN
+    exceptions <- Coordinates.Validation.Exceptions()
+
+    # WHEN
+    input <- FALSE
+    attribute <- 'attribute'
+
+    # THEN
+    input |> exceptions[['Attribute.NULL']](attribute) |> expect.no.error()
+  })
+  it("then an exceptions is thrown if input is TRUE",{
+    # GIVEN
+    exceptions <- Coordinates.Validation.Exceptions()
+
+    expected.exception <- "Attribute.NULL: 'attribute' does not exist."
+
+    # WHEN
+    input <- TRUE
+    attribute <- 'attribute'
+
+    # THEN
+    input |> exceptions[['Attribute.NULL']](attribute) |> expect.error(expected.exception)
+  })
+})
