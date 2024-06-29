9c45ca18115ee7854e5f19dd80987f326799c265
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 06:09:35 -0700

    VALIDATOR: offset |> validate[['exists']]() ThrowsExceptionIfOffsetIsNULL -> FAIL

--- tests/testthat/test-Offset.Validator.R
+++ tests/testthat/test-Offset.Validator.R
@@ -12,4 +12,46 @@ describe("When validators <- Offset.Validator()",{
     # THEN
     validators |> expect.list()
   })
+  it("then validators contains 'exists' validator",{
+    # When
+    validators <- Offset.Validator()
+
+    # Then
+    validators[['exists']] |> expect.function()
+  })
+})
+
+describe("When offset |> validate[['exists']]('Offset.NULL')",{
+  it("then no exception is thrown if offset is not NULL",{
+    # GIVEN
+    validate <- Offset.Validator()
+
+    # WHEN
+    offset <- list()
+
+    # THEN
+    offset |> validate[['exists']]('Offset.NULL') |> expect.no.error()
+  })
+  it("then a Offset.NULL exception is thrown if offset is NULL",{
+    # GIVEN
+    validate <- Offset.Validator()
+
+    expected.error <- "Offset.NULL: Offset does not exist."
+
+    # WHEN
+    offset <- NULL
+
+    # THEN
+    offset |> validate[['exists']]('Offset.NULL') |> expect.error(expected.error)
+  })
+  it("then offset is return if offset is not NULL",{
+    # GIVEN
+    validate <- Offset.Validator()
+
+    # WHEN
+    offset <- list()
+
+    # THEN
+    offset |> validate[['exists']]('Offset.NULL') |> expect.equal(offset)
+  })
 })
