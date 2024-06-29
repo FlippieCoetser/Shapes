422ea8ac0d917306114f2728163d1bac44a6b99a
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 04:58:06 -0700

    EXCEPTIONS: input |> exception[['Coordinates.NULL']]() ThrowsExceptionIfInputTRUE -> FAIL

--- tests/testthat/test-Coordinates.Validation.Exceptions.R
+++ tests/testthat/test-Coordinates.Validation.Exceptions.R
@@ -20,3 +20,30 @@ describe("When exceptions <- Coordinates.Validation.Exceptions()",{
     exceptions[['Coordinates.NULL']] |> expect.exist()
   })
 })
+
+describe("When input |> exceptions[['Coordinates.NULL']]()",{
+  it("then an exceptions is thrown if input is FALSE",{
+    # GIVEN
+    exceptions <- Coordinates.Validation.Exceptions()
+
+
+    # WHEN
+    input <- FALSE
+
+    # THEN
+    input |> exceptions[['Coordinates.NULL']]() |> expect.no.error()
+  })
+  it("then an exceptions is thrown if input is TRUE",{
+    # GIVEN
+    exceptions <- Coordinates.Validation.Exceptions()
+
+
+    expected.exception <- 'Coordinates.NULL: Coordinates does not exist.'
+
+    # WHEN
+    input <- TRUE
+
+    # THEN
+    input |> exceptions[['Coordinates.NULL']]() |> expect.error(expected.exception)
+  })
+})
