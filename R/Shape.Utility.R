96e8cc6557e410741b73f45fcdceb9c19d3597a1
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 06:28:59 -0700

    UTILITY: coordinates |> shape[['get.height']]() ThrowsExceptionIfCoordinatesIsInvalid -> PASS

--- R/Shape.Utility.R
+++ R/Shape.Utility.R
@@ -4,12 +4,14 @@ Shape.Utility <- \() {
   utilities <- list()
   utilities[['get.width']]     <- \(coordinates) {
     coordinates |> coordinates.validate[['Coordinates']]()
-    
+
     max.x <- coordinates[['x']] |> max()
     min.x <- coordinates[['x']] |> min()
     (max.x - min.x)
   }
   utilities[['get.height']]    <- \(coordinates) {
+    coordinates |> coordinates.validate[['Coordinates']]()
+    
     max.y <- coordinates[['y']] |> max()
     min.y <- coordinates[['y']] |> min()
     (max.y - min.y)
