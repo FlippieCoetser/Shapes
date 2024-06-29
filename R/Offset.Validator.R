da903774b8f08bf5faaa81421fd98e36df207b7d
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 06:09:40 -0700

    VALIDATOR: offset |> validate[['exists']]() ThrowsExceptionIfOffsetIsNULL -> PASS

--- R/Offset.Validator.R
+++ R/Offset.Validator.R
@@ -1,4 +1,11 @@
 Offset.Validator <- \() {
-  validators <- list()
+  exceptions <- Offset.Validation.Exceptions()
+
+  validators <- Validate::Validator()
+  validators[['exists']]      <- \(offset, exception) { 
+    offset |> validators[['is.not.NULL']]('') |> 
+      tryCatch(error = \(...){ TRUE |> exceptions[[exception]]()})
+    return(offset)
+  }
   return(validators)
 }
