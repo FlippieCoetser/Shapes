cee1d3602ccfdeae419940855717ee94f81b0d0e
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:42:25 -0700

    MODEL: data |> Offset.Model() ReturnsValidOffset -> FAIL

--- tests/testthat/test-Offset.Model.R
+++ tests/testthat/test-Offset.Model.R
@@ -3,3 +3,36 @@ describe('Offset.Model',{
     Offset.Model |> expect.exist()
   })
 })
+
+describe("When offset <- data |> Offset.Model()",{
+  it('then offset is a data.frame',{
+    # Given
+    data <- list(x = 1, y = 2)
+
+    # When
+    offset <- data |> Offset.Model()
+
+    # Then
+    offset |> expect.data.frame()
+  })
+  it('then offset contains a x attribute',{
+    # Given
+    data <- list(x = 1, y = 2)
+
+    # When
+    offset <- data |> Offset.Model()
+
+    # Then
+    offset[['x']] |> expect.exist()
+  })
+  it('then offset contains a y attribute',{
+    # Given
+    data <- list(x = 1, y = 2)
+
+    # When
+    offset <- data |> Offset.Model()
+
+    # Then
+    offset[['y']] |> expect.exist()
+  })
+})
