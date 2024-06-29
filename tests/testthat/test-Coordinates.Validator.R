bfe2ad73c15cac1e129658517a703bcd45503817
Flippie Coetser <flippie.coetser@gmail.com>
2024-06-29 05:05:57 -0700

    VALIDATOR: coordinates |> validate[['exists']]('Coordinates.NULL') ThrowsCoordinatesNULLExceptionIfCoordinatesIsNULL -> FAIL

--- tests/testthat/test-Coordinates.Validator.R
+++ tests/testthat/test-Coordinates.Validator.R
@@ -20,3 +20,38 @@ describe("When validators <- Coordinates.Validator()",{
     validators[['exists']] |> expect.function()
   })
 })
+
+describe("When coordinates |> validate[['exists']]('Coordinates.NULL')",{
+  it("then no exception is thrown if coordinates is not NULL",{
+    # GIVEN
+    validate <- Coordinates.Validator()
+
+    # WHEN
+    coordinates <- list()
+
+    # THEN
+    coordinates |> validate[['exists']]('Coordinates.NULL') |> expect.no.error()
+  })
+  it("then a Coordinates.NULL exception is thrown if coordinates is NULL",{
+    # GIVEN
+    validate <- Coordinates.Validator()
+
+    expected.error <- "Coordinates.NULL: Coordinates does not exist."
+
+    # WHEN
+    coordinates <- NULL
+
+    # THEN
+    coordinates |> validate[['exists']]('Coordinates.NULL') |> expect.error(expected.error)
+  })
+  it("then coordinates is return if coordinates is not NULL",{
+    # GIVEN
+    validate <- Coordinates.Validator()
+
+    # WHEN
+    coordinates <- list()
+
+    # THEN
+    coordinates |> validate[['exists']]('Coordinates.NULL') |> expect.equal(coordinates)
+  })
+})
